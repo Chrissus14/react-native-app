b6193085016b2f3578689ff7e4682d19da73356c
Chrissus14 <cjgm14@gmail.com>
2020-07-30 14:03:23 -0500

    improving ListingEditScreen

--- App.js
+++ App.js
@@ -1,29 +1,8 @@
 import React, { useEffect, useState } from "react";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
 import Screen from "./App/components/Screen";
-import { Button, Image } from "react-native";
-import ImageInput from "./App/components/ImageInput";
 import ImageInputList from "./App/components/ImageInputList";
+import ListingEditScreen from "./App/screens/ListingEditScreen";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-
-  return (
-    <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-      />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
