813df9493d5480c4cc630b131f2cf44ce01806a3
Chrissus14 <cjgm14@gmail.com>
2020-07-30 13:50:42 -0500

    FormImagePicker

---
+++ App/components/forms/FormImagePicker.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ImageInputList from "../ImageInputList";
+import ErrorMessage from "./ErrorMessage";
+import { useFormikContext } from "formik";
+
+const FormImagePicker = ({ name }) => {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  const imageUris = values[name];
+  const handleAdd = (uri) => {
+    setFieldValue(name, [...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setFieldValue(
+      name,
+      imageUris.filter((imageUri) => imageUri !== uri)
+    );
+  };
+  return (
+    <>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+};
+
+export default FormImagePicker;
+
+const styles = StyleSheet.create({});
